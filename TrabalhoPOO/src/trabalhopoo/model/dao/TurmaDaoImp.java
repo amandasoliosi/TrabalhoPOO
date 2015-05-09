2111fa2778c81b9f88f5a96673394974d8d734e1
KlausKly <klauskly_cw@hotmail.com>
2015-05-09 12:04:57 -0300

    Implementação do Turma Dao Impl. (Klaus)

--- TrabalhoPOO/src/trabalhopoo/model/dao/TurmaDaoImp.java
+++ TrabalhoPOO/src/trabalhopoo/model/dao/TurmaDaoImp.java
@@ -1,14 +1,94 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package trabalhopoo.model.dao;
+ package trabalhopoo.model.dao;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import trabalhopoo.model.pojo.Turma;
 
 /**
  *
- * @author Amanda
+ * @author Klaus
  */
-public class TurmaDaoImp {
-    
+
+//implementação da interface
+public class TurmaDaoImp implements InterfaceTurmaDaoImp{
+        private File arquivo = new File("ListaTurma.txt");
+	private ArrayList<Turma> lista = new ArrayList<Turma>();
+	private int ano;
+	private int periodo;
+	private String local;
+	private String horario;
+	private int numero_de_vagas;
+	private Turma turma;
+		
+	private FileWriter fw;
+	private BufferedWriter bw;
+	private FileReader fr;
+	private BufferedReader br;
+	Iterator<Turma> it;
+        
+        public ArrayList<Turma> ler() throws IOException{
+		if(arquivo.exists() == false){
+			arquivo.createNewFile();
+			return lista;
+		}
+		fr = new FileReader (arquivo);
+		br = new BufferedReader (fr);
+		while (br.ready()){
+			ano = br.readLine();
+			periodo = br.readLine();
+			local = br.readLine();
+			horario = br.readLine();
+			numero_de_vagas = br.readLine();
+			inserir(ano, periodo, local, horario, numero_de_vagas);
+		}
+		br.close();
+		fr.close();
+		return lista; 
+	}
+        
+        public ArrayList<Turma> getLista() {
+		return lista;
+	}
+        
+        //insere as turmas na lista em execução        
+        public void inserir (int ano, int periodo, String local, String horario, int numero_de_vagas){
+		turma = new Turma(ano, periodo, local, horario, numero_de_vagas);
+		lista.add(turma);
+	}
+        
+        //Salva um aluno no arquivo ListaAluno.
+        public boolean salvar(Turma turma) throws IOException{
+		if (turma == null) return false;
+		fw = new FileWriter(arquivo, true);
+		bw = new BufferedWriter (fw);
+		bw.write(turma.getAno());
+		bw.newLine();
+		bw.write(turma.getPeriodo());
+		bw.newLine();
+		bw.write(turma.getLocal());
+		bw.newLine();
+		bw.write(turma.getHorario());
+		bw.newLine();
+		bw.write(turma.getNumero_de_vagas());
+		bw.newLine();		
+		bw.close();
+		fw.close();
+		return true;
+	}
+        //Salva as turmas da lista no arquivo
+        public boolean salvar(ArrayList<Turma> lista) throws IOException{
+		if (lista.isEmpty() == true) return false;
+		fw = new FileWriter(arquivo);
+		it = lista.iterator();
+		while (it.hasNext()){
+			salvar(it.next());
+		}
+		return true;
+	}
 }
