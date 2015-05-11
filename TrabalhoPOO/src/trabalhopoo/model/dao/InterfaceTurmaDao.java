df14f07a11977b53efc162928f3bf6946fd98da9
Thiagotatagiba <thiagoctatagiba@gmail.com>
2015-05-10 23:51:35 -0300

    Klaus - InterfaceTurmaDao / TurmaDaoImp / TurmaView

--- TrabalhoPOO/src/trabalhopoo/model/dao/InterfaceTurmaDao.java
+++ TrabalhoPOO/src/trabalhopoo/model/dao/InterfaceTurmaDao.java
@@ -5,10 +5,18 @@
  */
 package trabalhopoo.model.dao;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import trabalhopoo.model.pojo.Turma;
 /**
  *
- * @author Amanda
+ * @author Klaus
  */
-public class InterfaceTurmaDao {
-    
+interface InterfaceTurmaDaoImp {
+    ArrayList<Turma> ler() throws IOException;
+    void inserir (int ano, int periodo, String local, 
+                String horario, int numero_de_vagas);
+    boolean salvar(Turma turma) throws IOException; // salva no arquivo
+    boolean salvar(ArrayList<Turma> lista) throws IOException; // salva no vetor	
 }
--- TrabalhoPOO/src/trabalhopoo/view/TurmaView.java
+++ TrabalhoPOO/src/trabalhopoo/view/TurmaView.java
@@ -32,7 +32,45 @@ public class TurmaView {
     }
     
  
-    //Fazer o cadastrar Turma
+    public void cadastrarTurma(){
+        System.out.println("Cadastro de turmas");
+        System.out.println("Informe o ano: ");
+        int ano = entrada.nextInt();
+/*	while (){                             Implemetar verificação
+		System.out.println("Ano de turma não aceito, digite o ano da 
+                                    turma novamente: ");
+		ano = entrada.nextInt();
+	}*/
+        System.out.println("Digite o periodo: ");
+	int periodo = entrada.nextInt();
+/*	while (){                             Implemetar verificação
+            System.out.println("Período inválido, digite-o novamente: ");
+		periodo = entrada.nextInt();
+	}*/
+        System.out.println("Digite o local: ");
+        String local = entrada.nextLine();
+/*      while(){
+                System.out.println("Local inválido, digite-o novamente: ");
+		local = entrada.nextLine();
+        */    
+        System.out.println("Digite o horário: ");
+        String horario = entrada.nextLine();
+/*      while(){
+                System.out.println("Horario de turma não aceito, digite o 
+                                    horario novamente: ");
+		horario = entrada.nextLine();
+        */
+        System.out.println("Digite o numero de vagas disponíveis: ");
+        int numero_de_vagas = entrada.nextInt();
+/*      while(){
+                System.out.println("Quandtidade de número de vagas incorreta, 
+                                    digite mais uma vez: ");
+		numero_de_vagas = entrada.nexInt();
+        */     
+        turmaDaoImp.inserir(ano, periodo, local, horario, numero_de_vagas);
+	return;	
+        dao.salvar(turmaDaoImp);
+    }
     
     public void listar(){
 
